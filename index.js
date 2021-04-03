37b62b032eda71336652e20a48fb4db18f4dd803
sm2_174 <krjhasub@gmail.com>
2021-04-03 17:56:31 +0530

    watherapp

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let loc =document.getElementById("location");
 let tempicon=document.getElementById("temp-icon");
 let tempvalue=document.getElementById("temp-value");
+let tempunit=document.getElementById("temp-unit");
 let climate =document.getElementById("climate");
 let iconfile;
 const searchInput=document.getElementById("search-input");
